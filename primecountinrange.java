ea428aa32604b8fd571bc916b27aae8440ee697d
saravana <saravanasaru14313@gmail.com>
2019-06-07 14:38:07 +0530

    Update and rename README.md to primecountinrange.java

---
+++ primecountinrange.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+class Primenumber
+
+{
+
+	public static void main(String[] args)
+ 
+	{
+	    
+		Scanner input=new Scanner(System.in);
+	
+        int a=input.nextInt();
+	
+        int b=input.nextInt();
+	
+        int flag=0;
+	
+        for(int i=a;i<=b;i++)
+
+	        {
+
+            		int temp=i;
+
+			int count=0;
+		        for(int j=1;j<b;j++)
+		        {
+	
+	                if(temp%j==0)
+		
+                {
+			                count=count+1;	
+		
+                }
+		        }
+	 	       if(count==2)
+	        	{
+	
+             	  	flag=flag+1;
+	        	}
+        	}
+
+        	System.out.println(flag);	
+
+	}
+
+}
